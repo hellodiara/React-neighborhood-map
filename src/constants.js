791e696e123b44c36fca407eeb90ad0621e71159
Diara-Bamba <diarabamba@gmail.com>
2018-11-15 10:59:03 -0500

    Added constants file

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+// You must use your own API keys for Google maps and Foursquare 
+
+// Google Maps API
+export const MAP_KEY = "";
+
+//Foursquare API
+export const FS_CLIENT = "";
+export const FS_SECRET = "";
+export const FS_VERSION = "";
